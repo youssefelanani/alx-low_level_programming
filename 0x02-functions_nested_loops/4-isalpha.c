15eb8bd96f51bbc52432a460985d994a83eebe89
youssefelanani <youssef.moustafa33@outlook.com>
2023-02-21 10:32:41 -0800

    o

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * isalpha - function that print lower and upper alphabet
+ *
+ * @c: parameter to br printed
+ *
+ * Return: 1 if it is alphabet lower or upper case
+ * and 0 if else
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
