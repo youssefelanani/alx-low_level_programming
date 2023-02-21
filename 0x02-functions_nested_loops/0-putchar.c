287daf5bccf5ed41154484d9ee99bb3aae14a255
youssefelanani <youssef.moustafa33@outlook.com>
2023-02-21 02:40:22 -0800

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <main.h>
 
 /**
  * main - main function
@@ -6,7 +7,14 @@
  */
 int main(void)
 {
-	_putchar('_putchar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
