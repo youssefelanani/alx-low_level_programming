be65bee0665ae29644533ecb902f4a63cb71dec7
youssefelanani <youssef.moustafa33@outlook.com>
2023-03-12 10:51:57 -0700

    v

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,10 @@
 #include "main.h"
-void _puts(char *str)
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
+void _puts(char *s)
+{
+	puts(s);
+}
