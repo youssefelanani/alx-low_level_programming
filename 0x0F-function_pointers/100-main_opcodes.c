6c8466bf48d34602b5a8d71af399736cb5efb329
youssefelanani <youssef.moustafa33@outlook.com>
2023-03-22 05:08:52 -0700

    w

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-	#include <stdlib.h>
+#include <stdlib.h>
 
 
-	/**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(int argc, char *argv[])
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 	{
 		int bytes, i;
 		char *arr;
@@ -45,4 +45,4 @@
 			printf("%02hhx ", arr[i]);
 		}
 		return (0);
-	}
+}
